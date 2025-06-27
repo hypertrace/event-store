76a8dfac29b6b4094d4c98328b4849bee6f765e7
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2025-06-26 20:49:57 -0400

    build: update plugins and repos (#12)
    * build: update plugins and repos
    
    * chore: update kafka

--- event-store/build.gradle.kts
+++ event-store/build.gradle.kts
@@ -5,18 +5,13 @@ plugins {
   id("org.hypertrace.jacoco-report-plugin")
 }
 
-repositories {
-  // Need this to fetch confluent's kafka-clients dependency
-  maven("http://packages.confluent.io/maven")
-}
-
 tasks.test {
   useJUnitPlatform()
 }
 
 dependencies {
   implementation("com.typesafe:config:1.3.2")
-  implementation(platform("org.hypertrace.core.kafkastreams.framework:kafka-bom:0.4.7"))
+  implementation(platform("org.hypertrace.core.kafkastreams.framework:kafka-bom:0.6.0"))
   implementation("org.apache.kafka:kafka-clients")
   implementation("commons-io:commons-io:2.14.0")
   implementation("org.slf4j:slf4j-api:1.7.30")
