f47388bf488773bf23b72eaccaa8f11ac698efb9
Aaron Steinfeld <aaron@traceable.ai>
2025-06-26 20:05:42 -0400

    chore: update kafka

--- event-store/build.gradle.kts
+++ event-store/build.gradle.kts
@@ -11,7 +11,7 @@ tasks.test {
 
 dependencies {
   implementation("com.typesafe:config:1.3.2")
-  implementation(platform("org.hypertrace.core.kafkastreams.framework:kafka-bom:0.4.7"))
+  implementation(platform("org.hypertrace.core.kafkastreams.framework:kafka-bom:0.6.0"))
   implementation("org.apache.kafka:kafka-clients")
   implementation("commons-io:commons-io:2.14.0")
   implementation("org.slf4j:slf4j-api:1.7.30")
