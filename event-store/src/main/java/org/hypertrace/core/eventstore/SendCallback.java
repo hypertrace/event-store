7c77c54d97788cb7297e2b6c78bc9314940815b6
GurtejSohi <gurtejsohi25@gmail.com>
2025-12-30 10:06:50 +0530

    feat: add callback support in send method of event producer (#16)

---
+++ event-store/src/main/java/org/hypertrace/core/eventstore/SendCallback.java
@@ -0,0 +1,17 @@
+package org.hypertrace.core.eventstore;
+
+/**
+ * A callback interface that allows clients to receive notifications when a send operation
+ * completes. This can be used to handle both successful sends and errors asynchronously.
+ */
+public interface SendCallback {
+
+  /**
+   * Called when a send operation completes.
+   *
+   * @param result The result of the send operation if successful, null if an exception occurred
+   * @param exception The exception thrown during the send operation, null if the send was
+   *     successful
+   */
+  void onCompletion(SendResult result, Exception exception);
+}
